a27daa7710d8c82c007fd69b0b7971a5f0a298e3
João <joaogui.silvae@gmail.com>
2026-05-05 00:14:07 -0300

    feat: add main application entry point with Fastify setup

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import "dotenv/config";
+
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifyApiReference from "@scalar/fastify-api-reference";
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { homeRoutes } from "./routes/home.js";
+
+const app = Fastify({ logger: true });
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Meu API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifyCors, {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+});
+
+await app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    sources: [
+      {
+        title: "Meu API",
+        url: "/swagger.json",
+      },
+    ],
+  },
+});
+
+await app.register(homeRoutes, { prefix: "/home" });
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/swagger.json",
+  handler: async () => app.swagger(),
+});
+
+await app.listen({ port: Number(process.env.PORT) || 8080 });
