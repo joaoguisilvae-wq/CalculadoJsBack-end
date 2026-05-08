1335d20f9fb9ef1b7806e71df3f152a4755798d2
João <joaogui.silvae@gmail.com>
2026-05-07 21:56:32 -0300

    feat(prisma): remove symbol field from Operation model

---
+++ prisma/migrations/20260507121620_remove_symbol_from_operation/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `symbol` on the `Operation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Operation" DROP COLUMN "symbol";
